2947ba223d557998951a31f6c4c1b5398e7be27e
Mamuka <mamuka.bagatelia2025@campus-eni.fr>
2025-08-01 16:33:11 +0200

    premier commit

---
+++ src/Controller/PokemonController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pokemon;
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Node\Scalar\String_;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
+
+final class PokemonController extends AbstractController
+{
+    #[Route('/', name: 'pokemon_home')]
+    public function home(): Response
+    {
+        return $this->render('pokemon/home.html.twig');
+    }
+
+    #[Route('/list', name: 'pokemon_list')]
+    public function list(PokemonRepository $pokemonRepository): Response
+    {
+        $pokemons = $pokemonRepository->findAll();
+
+        return $this->render('pokemon/list.html.twig', ['pokemons' => $pokemons]);
+    }
+
+    #[Route('/list/{id}', name: 'pokemon_detail', requirements: ['id' => '\d+'])]
+    public function detail(Pokemon $pokemon): Response
+    {
+        return $this->render('pokemon/detail.html.twig', ['pokemon' => $pokemon]);
+    }
+
+    #[Route('/capture/{id}', name: 'pokemon_capture', requirements: ['id' => '\d+'])]
+    public function capture(Pokemon $pokemon, PokemonRepository $pokemonRepository, EntityManagerInterface $em): Response
+    {
+        $pokemon->setEstCapture(!$pokemon->isEstCapture());
+        $em->flush();
+
+        $pokemons = $pokemonRepository->findAll();
+        return $this->render('pokemon/list.html.twig', ['pokemons' => $pokemons]);
+    }
+
+    #[Route('/tri/{param}', name: 'pokemon_tri')]
+    public function tri(string $param, PokemonRepository $pokemonRepository, EntityManagerInterface $em): Response
+    {
+        if ($param == 'name'){
+            $pokemons = $pokemonRepository->sortByName();
+        }elseif ($param == 'capture'){
+            $pokemons = $pokemonRepository->sortByCapture();
+        }
+
+        return $this->render('pokemon/list.html.twig', ['pokemons' => $pokemons]);
+    }
+
+
+
+}
